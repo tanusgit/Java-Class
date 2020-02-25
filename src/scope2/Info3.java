2ff94e88ec338c641ba911aec05144900874c84a
tanu <24976766+tanusgit@users.noreply.github.com>
2020-02-25 11:24:36 -0800

    modifiers

--- src/scope2/Info3.java
+++ src/scope2/Info3.java
@@ -2,6 +2,12 @@ package scope2;
 
 import scope1.Info;
 
+//how to import a package
+//import package name then class name
+
+//import homework.ArrayString; cannot be imported as it is not in the same folder
+//we can only import packages if they are in the same folder
+import stringBuffer.Notes;
 public class Info3 {
 	public void show(){
 		//access everything within class
