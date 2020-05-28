c07b68af331115dd502d008a625cf706ef5a6a3e
tanu <24976766+tanusgit@users.noreply.github.com>
2020-05-28 13:28:44 -0700

    even nums while loop

---
+++ src/interviewQuestions/Main.java
@@ -0,0 +1,18 @@
+package interviewQuestions;
+
+public class Main {
+public static void main(String[] args) {
+	even();
+
+}
+
+public static  void even() {
+	
+        int i = 1;
+        System.out.println("Even numbers");
+        while(i<=10){
+            System.out.println(i*2);
+           i++;
+        }
+    }
+}
