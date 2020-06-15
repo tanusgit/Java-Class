9878a31c839551623662438a2025b443a7fb0932
tanu <24976766+tanusgit@users.noreply.github.com>
2020-06-14 20:06:49 -0700

    find functions

---
+++ src/interviewQuestions/FindFunctions.java
@@ -0,0 +1,58 @@
+package interviewQuestions;
+/*
+ * 
+//function signature
+//return statement 
+//return type like void and int can determine if it is a function
+ */
+/*
+ * tokenize
+ * brace matching
+ * Assumption: Program has no comments
+ * No generics
+ * no throw statement
+ * every function has public/private qualifier
+ */
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class FindFunctions {
+	public static void main(String[] args) throws FileNotFoundException {
+		int m = find("/home/td/Desktop/Java/AtmProject/src/interviewQuestions/FindFunctions.java");
+		System.out.println(m);
+
+	}
+	public static int find(String filename) throws FileNotFoundException {
+		int num = 0;
+		int paren1 = '(';
+		int paren2 = ')';
+		int lineNum = 0;
+
+		FileInputStream fis = new FileInputStream(filename);
+		Scanner scanner = new Scanner(fis);
+		while (scanner.hasNextLine()) {
+			String line = scanner.nextLine();
+			if (line.contains("return")||line.contains("void")) {
+				System.out.println("line at: "+ line);
+				lineNum++;
+			}
+			//System.out.println(line);
+		}
+		return lineNum;
+	}
+	
+	
+	
+
+	public void add() {
+
+	}
+
+	public int sub() {
+		return 0;
+	}
+
+}
